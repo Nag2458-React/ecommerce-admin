5df99b4bd0b11c64ee7173dc280b3ecc181e3de3
Nag2458-React <nagababu.raavi@aptonline.dev>
2026-07-16 11:29:34 +0530

    Add files via upload

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -5,9 +5,17 @@ import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 import { signOut } from "firebase/auth";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 
+import { db } from "../firebase/firebase";
 import { auth } from "../firebase/firebase";
-
+import logo from "../../public/images/logo.png"
 import {
   FaHome,
   FaHeart,
@@ -16,6 +24,7 @@ import {
   FaClipboardList,
   FaSignOutAlt,
   FaDownload,
+  FaBell,
 } from "react-icons/fa";
 import { useWishlist } from "../context/WishlistContext";
 import { useCart } from "../context/CartContext";
@@ -27,6 +36,10 @@ const Navbar = () => {
   const { cart } = useCart();
   const [cartCount, setCartCount] = useState(0);
   const [wishlistCount, setWishlistCount] = useState(0);
+const [notifications, setNotifications] = useState([]);
+const [showNotifications, setShowNotifications] = useState(false);
+
+
   useEffect(() => {
     const totalQty = cart.reduce((sum, item) => sum + Number(item.qty || 1), 0);
 
@@ -35,6 +48,28 @@ const Navbar = () => {
   useEffect(() => {
     setWishlistCount(wishlist.length);
   }, [wishlist]);
+
+  useEffect(() => {
+  if (!user) return;
+
+  const q = query(
+    collection(db, "notifications"),
+    where("userEmail", "==", user),
+    orderBy("createdAt", "desc")
+  );
+
+  const unsubscribe = onSnapshot(q, (snapshot) => {
+    const list = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setNotifications(list);
+  });
+
+  return () => unsubscribe();
+}, [user]);
+
 
   const handleLogout = async () => {
     try {
@@ -58,10 +93,10 @@ const Navbar = () => {
     }
   };
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-dark shadow-sm p-0">
       <div className="container">
-        <NavLink className="navbar-brand fw-bold" to="/">
-          Shop
+        <NavLink className="navbar-brand fw-bold p-0" to="/" style={{margin:"0 "}}>
+         <img src={logo} style={{width:"80px"}}></img> 
         </NavLink>
 
         <button
@@ -193,12 +228,96 @@ const Navbar = () => {
                 Profile
               </NavLink>
             </li>
-           
+          
           </ul>
 
           <div className="d-flex align-items-center">
             <span className="e-text me-3">{user}</span>
+          <div className="position-relative me-3">
 
+  <FaBell
+    size={22}
+    style={{ cursor: "pointer", color: "#fff" }}
+    onClick={() =>
+      setShowNotifications(!showNotifications)
+    }
+  />
+
+  {notifications.filter((n) => !n.isRead).length > 0 && (
+
+    <span
+      className="badge bg-danger position-absolute"
+      style={{
+        top: "-8px",
+        right: "-8px",
+      }}
+    >
+      {notifications.filter((n) => !n.isRead).length}
+    </span>
+
+  )}
+{showNotifications && (
+  <div
+    className="card shadow position-absolute"
+    style={{
+      width: "320px",
+      right: "0",
+      top: "35px",
+      zIndex: 9999,
+      maxHeight: "400px",
+      overflowY: "auto",
+    }}
+  >
+    <div className="card-header fw-bold">
+      🔔 Notifications
+    </div>
+
+    <div className="card-body p-0">
+      {notifications.length === 0 ? (
+        <p className="text-center p-3 mb-0">
+          No Notifications
+        </p>
+      ) : (
+        notifications.map((item) => (
+         <div
+  key={item.id}
+  className="border-bottom p-3 d-flex align-items-center"
+  style={{
+    background: item.isRead ? "#fff" : "#f8f9fa",
+  }}
+>
+  <img
+    src={item.productImage}
+    alt=""
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "8px",
+      objectFit: "cover",
+      marginRight: "10px",
+    }}
+  />
+
+  <div style={{ flex: 1 }}>
+    <div className="fw-bold">
+      {item.productName}
+    </div>
+
+    <small>{item.message}</small>
+
+    <br />
+
+    <small className="text-muted">
+      Order ID : {item.orderId.slice(0, 8)}
+    </small>
+  </div>
+</div>
+        ))
+      )}
+    </div>
+  </div>
+)}
+</div>
             <button
               type="button"
               className="btn btn-danger btn-log"
